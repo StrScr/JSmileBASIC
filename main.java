df41a77873c682389a800455cfb8d59f17776613
StrScr <andres.mejia@unitec.edu>
2018-06-03 11:48:23 -0600

    Recover tree from parser into Main. Used to do everything from parser, which was a bit ugly. Now saves the AST as publicly accessible member of parser instance.

--- main.java
+++ main.java
@@ -1,12 +1,14 @@
 import java.io.*;
 
-public class Main{
+public class Main{ 
     public static void main(String argv[]){
         try{
             parser p = new parser(new Lexer(new FileReader(argv[0])));
             Object result = p.parse().value;
+            Node tree = p.AbstractTree;
+            tree.printTree(0);
         }catch(Exception e){
-            System.out.println("(main) Error.");
+            System.out.println("(main) Error fatal de compilación.");
         }
     }
 }
