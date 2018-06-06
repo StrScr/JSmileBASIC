a1c21d343b25a40a013badb7ac5aecb64cd15e61
StrScr <andres.mejia@unitec.edu>
2018-06-06 06:29:39 -0600

    Catch specific exceptions. Catch index-out-of-bounds and file-not-found exceptions individually.

--- main.java
+++ main.java
@@ -5,8 +5,13 @@ public class Main{
         try{
             parser p = new parser(new Lexer(new FileReader(argv[0])));
             Object result = p.parse().value;
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Main: No se especifico un archivo.");
+        }catch(FileNotFoundException e){
+            System.out.println("Main: No se encontro el archivo.");
         }catch(Exception e){
-            System.out.println("(main) Error fatal de compilación.");
+            System.out.println("Main: Error fatal desconocido de compilacion.");
+            e.printStackTrace();
         }
     }
 }
